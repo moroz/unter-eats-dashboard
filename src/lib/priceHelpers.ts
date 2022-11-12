7501e9f6797ef5ecaf8855abd49f8cd24db96c66
Karol Moroz <k.j.moroz@gmail.com>
2022-11-12 18:11:06 +0100

    Fix the build

--- src/lib/priceHelpers.ts
+++ src/lib/priceHelpers.ts
@@ -1,4 +1,5 @@
-export const formatPrice = (price: number | string) => {
+export const formatPrice = (price: number | string | null | undefined) => {
+  if (price === null || price === undefined) return null;
   const isInt = Math.round(Number(price)) == Number(price);
   if (isInt) return `${Number(price)} zł`;
   return Number(price).toFixed(2).replace(".", ",") + " zł";
